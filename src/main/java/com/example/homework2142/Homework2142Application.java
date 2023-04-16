796c5905e408f2749cb4a73d513a6e8fa0ebd7b8
CosmoBlack <soulker_mozyr@yahoo.com>
2023-04-16 21:33:30 +0200

    Initial commit

---
+++ src/main/java/com/example/homework2142/Homework2142Application.java
@@ -0,0 +1,13 @@
+package com.example.homework2142;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework2142Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework2142Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework2142/Homework2142ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2142;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework2142ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
